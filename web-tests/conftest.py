80505a02dbf8dcbeb1a78f3e1f32e49942b68fd9
Eliaspio23 <seu_email@gmail.com>
2026-05-04 21:44:03 -0300

    fix: add --disable-gpu and --remote-debugging-port flags to Chrome

--- web-tests/conftest.py
+++ web-tests/conftest.py
@@ -9,9 +9,12 @@ def driver():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--remote-debugging-port=9222")
 
     d = webdriver.Chrome(options=options)
-    d.implicitly_wait(5)
+    d.implicitly_wait(8)
     yield d
     d.quit()
